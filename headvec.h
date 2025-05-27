c4b602a11c0b69b2e9dd586291de104233b2d623
NdetecT306 <nstasya777@mail.ru>
2025-05-27 08:16:55 +0700

    Create headvec.h

---
+++ headvec.h
@@ -0,0 +1,8 @@
+#include<vector>
+#include<string>
+using namespace std;
+vector<int> continued_fraction(double x, int iterations);
+vector<int> primeFactors(int n);
+string Decrypt(const vector<pair<int, int>>& ciphertext, int x, int p);
+int LineFrac();
+bool Primitive(int g, int p);
