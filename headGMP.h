f8c951fc325dc67109a73d9b0ffa2d614a3e800e
NdetecT306 <nstasya777@mail.ru>
2025-05-28 10:54:18 +0700

    Create headGMP.h

---
+++ headGMP.h
@@ -0,0 +1,13 @@
+#include <gmp.h>
+#include <random>
+#include <algorithm>
+#include <numeric>
+#include <chrono>
+#include <gmpxx.h>
+using namespace std;
+void RandomNumbers_gmp(mpz_t result, mpz_t start, mpz_t end);
+void findPrimitiveRoot(mpz_t result, mpz_t p);
+string Decrypt(const vector<pair<mpz_class, mpz_class>>& ciphertext, mpz_t x, mpz_t p);
+int ElGamal_gmp();
+void Bin(mpz_t result, mpz_t a, mpz_t x, mpz_t p);
+bool prostota_gmp(mpz_t p);
